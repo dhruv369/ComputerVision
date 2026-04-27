ef13dc0196ac7e278fc61714f5d5a131edb51bf5
Dhruv <dhruv.vyas3690@gmail.com>
2026-04-27 23:21:28 +0200

    fix(model): Make minor changes into model structure for learning purpose.

--- mnist/mnist_model.py
+++ mnist/mnist_model.py
@@ -1,30 +1,29 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from mnist_datasets import MNISTDataset
 from torchsummary import summary
 
 # Define the MNIST model class
 class MNISTModel(nn.Module):
     def __init__(self):
         super().__init__()
-        # input size is 28x28x1
+        # Conv layers
         self.conv1 = nn.Conv2d(1, 16, kernel_size=3)
-        # input size is 26x26x16 after conv1, then max pooling reduces it to 13x13x16
         self.conv2 = nn.Conv2d(16, 32, kernel_size=3)
-        # input size is 11x11x32 after conv2, then max pooling reduces it to 5x5x32
-        self.fc1 = nn.Linear(32 * 5 * 5, 32)
-        # output size is 64, then fully connected layer reduces it to 10 for the 10 classes
+        self.conv3 = nn.Conv2d(32, 64, kernel_size=3)
+        # Fc layers
+        self.fc1 = nn.Linear(64 * 11 * 11, 32)
         self.fc2 = nn.Linear(32, 10)
+        self.relu = nn.ReLU
 
     def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.max_pool2d(x, 2)
-        x = F.relu(self.conv2(x))
-        x = F.max_pool2d(x, 2)
-        x = x.view(-1, 32 * 5 * 5)
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
-        x = F.log_softmax(x, dim=1)
+        x = F.relu(self.conv1(x)) # [B,16,26,26]
+        x = F.relu(self.conv2(x)) # [B,32,24,24]
+        x = F.relu(self.conv3(x)) # [B,64,22,22]
+        x = F.max_pool2d(x, 2)    # [B,64,11,11]
+        x  = torch.flatten(x,1)     # [B, 7744 ] 
+        x = F.relu(self.fc1(x))     
+        x = self.fc2(x)           # [B, 10 ]
         return x    
     
     def model_summary(self):
